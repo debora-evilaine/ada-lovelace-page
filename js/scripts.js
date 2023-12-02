f3f63678e0c2cdd64d3763f27bc1bc6b644321d9
debora-evilaine <sousxdebora@gmail.com>
2023-12-02 14:33:36 -0300

    Creaing front page

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function revealText() {
+    document.getElementById("initialText").style.opacity = 0;
+    document.getElementById("revealedText").style.opacity = 1;
+  }
+  
+  function hideText() {
+    document.getElementById("initialText").style.opacity = 1;
+    document.getElementById("revealedText").style.opacity = 0;
+  }
+  
